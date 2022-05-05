93b8684046c85eb39a8a12e212db4ac4156963d2
Akanksha <b.akanksha6@gmail.com>
2022-05-05 14:24:39 +0530

    context used

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,10 +1,29 @@
 import React from "react";
-import useApi from "../../utils/useApi";
+import { useStateValue } from "../../context/StateProvider";
 import FavCard from "../favCard";
 import "./movies.css";
 
 const Movies = () => {
-  const { loading, data } = useApi();
+  React.useEffect(() => {
+    fetchData();
+  });
+
+  const [{ loading, data }, dispatch] = useStateValue();
+
+  const fetchData = () => {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    const keyword = alphabet[Math.floor(Math.random() * alphabet.length)];
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_APIKEY}&language=en-US&sort_by=popularity.desc&page=1&with_keywords=${keyword}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        dispatch({ type: "SET_LOADING", payload: false });
+        dispatch({ type: "GET_DATA", payload: json.results });
+      });
+  };
 
   return (
     <div className="movies">
